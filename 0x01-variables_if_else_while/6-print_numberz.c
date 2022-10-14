2eddb02e1b8355eb0526b89208d26d4b00e2c20d
Nwike Vincent Ikechukwu <nwikeikechukwu@gmaiil.com>
2022-10-14 22:12:30 +0000

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ **main - Print base 10 digit using puchar
+ **
+ **Return: Always 0 (Success)
+ **/
+int main(void)
+{
+
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i%10 + '0');
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
