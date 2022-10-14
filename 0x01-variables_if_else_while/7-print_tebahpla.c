c8959e66c17eba72b5a5fd5f8c987680814b28d3
Nwike Vincent Ikechukwu <nwikeikechukwu@gmaiil.com>
2022-10-14 22:22:43 +0000

    7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ **main - Print numbers in reverse
+ **
+ **Return: Always 0 (Success)
+ **/
+int main(void)
+{
+
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
