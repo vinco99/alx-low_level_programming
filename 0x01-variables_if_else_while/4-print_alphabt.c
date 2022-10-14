a6bc98405a018ae1624283e235c9a8a83cd93f0c
Nwike Vincent Ikechukwu <nwikeikechukwu@gmaiil.com>
2022-10-14 21:41:06 +0000

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ **main - Print lowercase alphabets except q and e
+ **
+ **Return: Always 0 (Success)
+ **/
+int main(void)
+{
+
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
