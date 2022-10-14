75f90205f01339c91d1ba4307c194715810f0cd3
Nwike Vincent Ikechukwu <nwikeikechukwu@gmaiil.com>
2022-10-14 23:29:20 +0000

    100-print_comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ **main - Print all possible different combinations of two digits
+ **
+ **Return: Always 0 (Success)
+ **/
+int main(void)
+{
+	int x, y;
+
+	for (x = 0; x < 9; x++)
+	{
+		for (y = 1; y < 10; y++)
+		{
+			putchar(x % 10 + '0');
+			putchar(y % 10 + '0');
+
+			if (x == 8 && y == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
