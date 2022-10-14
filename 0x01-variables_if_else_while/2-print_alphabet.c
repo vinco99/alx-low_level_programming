86098b09e8c82aefaa0186fd7227e28374d2cfc1
Nwike Vincent Ikechukwu <nwikeikechukwu@gmaiil.com>
2022-10-14 20:54:56 +0000

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ **main - Print alphabets
+ **
+ **Return: Always 0 (Success)
+ **/
+int main(void)
+{
+
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		putchar(ch);
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
