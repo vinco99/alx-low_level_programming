d1f034f5edb5c38848fb494a2cec4fb769b5ab6a
Nwike Vincent Ikechukwu <nwikeikechukwu@gmaiil.com>
2022-10-15 01:04:11 +0000

    update

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,18 +11,15 @@ int main(void)
 
 	for (x = 0; x < 9; x++)
 	{
-		for (y = 1; y < 10; y++)
+		for (y = x + 1; y < 10; y++)
 		{
-			if (x != y)
-			{
-				putchar(x % 10 + '0');
-				putchar(y % 10 + '0');
+			putchar(x % 10 + '0');
+			putchar(y % 10 + '0');
 
-				if (x == 8 && y == 9)
-					continue;
-				putchar(',');
-				putchar(' ');
-			}
+			if (x == 8 && y == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
 		}
 	}
 
