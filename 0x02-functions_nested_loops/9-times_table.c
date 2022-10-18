bcc7cfb59e2f506ac18bab33509a6b1076f4c9a8
Nwike Vincent Ikechukwu <nwikeikechukwu@gmaiil.com>
2022-10-18 23:41:14 +0000

    update

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,13 +5,14 @@
  */
 void times_table(void)
 {
-	int x, y;
+	int x, y, z;
+	z = x * y;
 
 	for (x = 0; x < 10; x++)
 	{
 		for (y = 0; y < 10; y++)
 		{
-			_putchar((x * y) + '0');
+			_putchar(z);
 
 			if (y != 9)
 			{
