c55a82e69abcc61a8538826592eb04a38409aa0b
Nwike Vincent Ikechukwu <nwikeikechukwu@gmaiil.com>
2022-10-20 20:23:42 +0000

    update

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - Prints numbers
+ */
+void more_numbers(void)
+{
+	int x, y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y <= 14; y++)
+		{
+			if (y >= 10)
+			{
+				_putchar('1');
+			}
+			_putchar(y % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -20,4 +20,6 @@ void print_numbers(void);
 
 void print_most_numbers(void);
 
+void more_numbers(void);
+
 #endif
