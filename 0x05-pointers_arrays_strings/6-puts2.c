6b44dccd86fb31ac36ad57971af5a42cb83117fa
Nwike Vincent Ikechukwu <nwikeikechukwu@gmaiil.com>
2022-10-25 10:56:13 +0000

    update

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints every other character of a string
+ * @str: the string
+ */
+void puts2(char *str)
+{
+	int i, j;
+
+	j = 0;
+	while (str[j] != '\0')
+	{
+		j++;
+	}
+	for (i = 0; i <= j; i = i + 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * puts_half - Prints half of a string
+ * @str: the string parameter
+ */
+void puts_half(char *str)
+{
+	int i, n;
+
+	n = 0;
+	while (str[n] != '\0')
+	{
+		n++;
+	}
+	for (i = n / 2; i < n; i++)
+	{
+		if (n % 2 != 0)
+		{
+			_putchar(str[i]);
+			_putchar(str[n]);
+		}
+		else
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
