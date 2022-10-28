bd81f8200728b448df5ab8acaf006269c05c4891
Nwike Vincent Ikechukwu <nwikeikechukwu@gmaiil.com>
2022-10-28 15:49:28 +0000

    Update

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_number - Print number using putchar
+ * @n: the number
+ */
+void print_number(int n)
+{
+	int i;
+
+	if (n <= 1000)
+	{
+		_putchar(n + '0');
+		
+	}
+	else
+	{
+
+	}
+}
