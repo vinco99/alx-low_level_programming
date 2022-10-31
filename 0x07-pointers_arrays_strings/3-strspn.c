b5d0cef7259e48b9b9a2cbb8507638b8014cfdfb
Nwike Vincent Ikechukwu <nwikeikechukwu@gmaiil.com>
2022-10-31 20:59:49 +0000

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strspn - Gets the length of prifixes of substring
+ * @s: the string
+ * @accept: the 
+ *
+ * Returns: unsigned int.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	return (strspn(s, accept));
+}
