2b8b031616f794d492319d997876999ed1343409
Nwike Vincent Ikechukwu <nwikeikechukwu@gmaiil.com>
2022-10-31 22:20:01 +0000

    update

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -6,16 +6,13 @@
  */
 void print_chessboard(char (*a)[8])
 {
-	int i, j, k;
+	int i, k;
 
 	for (i = 0; i < 8; i++)
 	{
-		for (k = 0; k < j; k++)
+		for (k = 0; k < 8; k++)
 		{
-			if (a[k] != ' ')
-			{
-				_putchar(a[i][j]);
-			}
+				_putchar(a[i][k]);
 		}
 
 		_putchar('\n');
