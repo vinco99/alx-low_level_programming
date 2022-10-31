dc2516f6562a5d59e0b03fef6f2a010df04dfc4b
Nwike Vincent Ikechukwu <nwikeikechukwu@gmaiil.com>
2022-10-31 22:56:19 +0000

    update

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -7,21 +7,15 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int i, j, k, s, p;
+	int i, p, s;
 
-	s = 0;
 	p = 0;
+	s = 0;
 
 	for (i = 0; i < size; i++)
 	{
-		k = (i * size) + i;
-		p += *(a + k);
-	}
-
-	for (j = 0; j < size; j++)
-	{
-		k = (j * size) + (size - 1 - j);
-		s = *(a + k);
+		p += [i][i];
+		s += [i][size - i - 1];
 	}
 	printf("%i, %i\n", p, s);
 }
