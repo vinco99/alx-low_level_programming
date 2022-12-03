5bbd3486f3d0a921205ba63251201f1cc3220321
Nwike Vincent Ikechukwu <nwikeikechukwu@gmaiil.com>
2022-12-03 15:09:40 +0000

    update

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 /**
  * main - prints the name of the file it was compiled from and a new line
+ *
  * Return: 0 (Success Always)
  */
 int main(void)
 {
 	printf("%s\n", __FILE__);
-	return(0);
+
+	return (0);
 }
