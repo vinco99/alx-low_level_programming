aba2cfcdfd649dd3062af3260774cec03ed00468
Nwike Vincent Ikechukwu <nwikeikechukwu@gmaiil.com>
2023-01-15 01:36:27 +0000

    update

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,7 +1,21 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef LIST_H
+#define LIST_H
 
 #include <stddef.h>
+/**
+ * struct dlistint_s - a double linked list
+ *  @n: data
+ *  @prev: points to the previous node
+ *  @next: points to the next node
+ *
+ *  Description: doubly linked list
+ */
+typedef struct dlistint_s
+{
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
+} dlistint_s;
 
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
@@ -13,19 +27,4 @@ int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 
-/*
- * struct dlistint_s - a double linked list
- * @n: data
- * @prev: points to the previous node
- * @next: points to the next node
- *
- * Description: doubly linked list
- */
-typedef struct dlistint_s
-{
-	int n;
-	struct dlistint_s *prev;
-	struct dlistint_s *next;
-} dlistint_s;
-
-#endif
+#endif/*LIST_H*/
